28fd8f843a3cb70dfaeeaa9f0922b793804385c5
Richard Murray-Clark <richardmurray-clark@hotmail.com>
2015-05-05 19:19:03 +0100

    Added operator calculation and equals

--- calc.js
+++ calc.js
@@ -1,17 +1,48 @@
 var numbers = document.getElementsByClassName('number')
 var operators = document.getElementsByClassName('operator')
+var result= 0; 
 
-var num1 = '';
-var op1 = '';
-var start;
+// document.getElementsByClassName('input').value
+
+var num2 = '';
+var opType = '';
+var start = '';
+var eq = '';
 
 for(var n=0;n<numbers.length;n++)
 numbers[n].addEventListener('click', function(event){
-num1 += this.value;
+num2 += this.value;
 });
 
 for(var i=0;i<operators.length;i++)
 operators[i].addEventListener('click', function(event){
-start = num1;
-op1 =this.value;
+start = num2;
+opType =this.value;
+num2 = '';
 });
+
+document.getElementsByClassName('eq')[0].addEventListener('click', function(event){
+    eq = this.value;
+    start = parseInt(start);
+    num2 = parseInt(num2);
+    switch(opType) {
+      case "-":
+        result = (start - num2);
+        break;
+        
+      case"x":
+        result = (start * num2);
+        break;
+          
+      case"/":
+        result = (start / num2);
+        break;
+          
+      case"+":
+        result = (start + num2);
+        break;
+    }
+  
+  });
+
+
